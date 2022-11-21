2acd3c9ec98555c30269fd47fffcde44bd390bf0
sungjw0408 <sungjw0408@gmail.com>
2022-11-21 20:38:39 +0900

    성진욱: 3회차 소스코드 업로드

---
+++ sungjw0408/lesson3/model/Main.java
@@ -0,0 +1,46 @@
+package model;
+
+class Student{
+    private String studentNo;
+    private String name;
+    private String gender;
+
+    Student(){};
+
+    Student(String studentNo, String name, String gender){
+        this.studentNo = studentNo;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void getStudentNo(){
+        System.out.print(this.studentNo+" ");
+    }
+
+    public void getName(){
+        System.out.print(this.name+" ");
+    }
+
+    public void getGender(){
+        System.out.println(this.gender);
+    }
+
+    public void print(){
+        System.out.print(this.studentNo);
+        System.out.print(this.name);
+        System.out.print(this.gender);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Student kim = new Student("32222244 ","성진욱 ","남자 ");
+
+        System.out.println("----------get메소드----------");
+        kim.getStudentNo();
+        kim.getName();
+        kim.getGender();
+
+        System.out.println("----------print메소드----------");
+        kim.print();
+    }
+}
