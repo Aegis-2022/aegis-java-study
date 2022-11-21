ca9861126abb966ced5db653c7a63d346256289c
hanssoy <soyeonesther@naver.com>
2022-11-21 18:45:33 +0900

    2회차 소스코드 업로드

---
+++ soyeon/lesson2/lesson2Test2.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class lesson2Test2 {
+    public static void main(String[] args) {
+        for (int i = 9; i > 0; i--) {
+            for (int j = 9; j > 0; j--) {
+                System.out.println(i + "*" + j + "=" + i * j);
+            }
+        }
+
+    }
+}
